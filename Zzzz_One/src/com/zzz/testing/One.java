2e17df5ebaf1d532d98f32949e96dc78c8312d10
Moulirajsmr <mouli.rock07@gmail.com>
2023-01-19 21:44:49 +0530

    First

---
+++ Zzzz_One/src/com/zzz/testing/One.java
@@ -0,0 +1,15 @@
+package com.zzz.testing;
+
+public class One {
+	
+	private void Mouli() {
+		System.out.println("hero");
+	}
+	
+	public static void main(String[] args) {
+		One k = new One();
+		k.Mouli();
+	}
+	
+
+}
