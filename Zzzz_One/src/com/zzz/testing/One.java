345ffb491eb1d346c7889b14ef2c6e065be26d9e
Moulirajsmr <mouli.rock07@gmail.com>
2023-01-19 22:05:59 +0530

    update

--- Zzzz_One/src/com/zzz/testing/One.java
+++ Zzzz_One/src/com/zzz/testing/One.java
@@ -5,10 +5,15 @@ public class One {
 	private void Mouli() {
 		System.out.println("hero");
 	}
+	private void Raj() {
+		System.out.println("Thala");
+
+	}
 	
 	public static void main(String[] args) {
 		One k = new One();
 		k.Mouli();
+		k.Raj();
 	}
 	
 
